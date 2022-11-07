48aff748155ba5ee3fc40ee5501af028980998a7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-11-07 09:56:38 +0100

    add support for catalog offsets

--- placements/default.lua
+++ placements/default.lua
@@ -1,10 +1,19 @@
 
 building_lib.register_placement("default", {
 	place = function(self, mapblock_pos, building_def, rotation, callback)
-		local catalog = mapblock_lib.get_catalog(building_def.catalog)
+		local catalog
+		local offset = {x=0, y=0, z=0}
+		if type(building_def.catalog) == "table" then
+			catalog = mapblock_lib.get_catalog(building_def.catalog.filename)
+			offset = building_def.catalog.offset
+		else
+			catalog = mapblock_lib.get_catalog(building_def.catalog)
+		end
 		local size = self.get_size(self, mapblock_pos, building_def, 0)
-		local catalog_pos1 = {x=0, y=0, z=0}
+
+		local catalog_pos1 = vector.add({x=0, y=0, z=0}, offset)
 		local catalog_pos2 = vector.add(catalog_pos1, vector.add(size, -1))
+
 		local iterator = mapblock_lib.pos_iterator(catalog_pos1, catalog_pos2)
 
 		local worker
@@ -14,10 +23,13 @@ building_lib.register_placement("default", {
 				return callback()
 			end
 
-			-- transform catalog position
-			local rotated_catalog_pos = mapblock_lib.rotate_pos(catalog_pos, catalog_pos2, rotation)
+			-- transform catalog position relative to offset
+			local rel_pos = vector.subtract(catalog_pos, offset)
+			local max_pos = vector.subtract(catalog_pos2, offset)
+			local rotated_rel_catalog_pos = mapblock_lib.rotate_pos(rel_pos, max_pos, rotation)
+
 			-- translate to world-coords
-			local world_pos = vector.add(mapblock_pos, rotated_catalog_pos)
+			local world_pos = vector.add(mapblock_pos, rotated_rel_catalog_pos)
 
 			catalog:deserialize(catalog_pos, world_pos, {
 				transform = {
@@ -35,11 +47,22 @@ building_lib.register_placement("default", {
 		worker()
 	end,
 	get_size = function(_, _, building_def, rotation)
-		local catalog = mapblock_lib.get_catalog(building_def.catalog)
-		return mapblock_lib.rotate_size(catalog:get_size(), rotation)
+		local size
+		if type(building_def.catalog) == "table" then
+			size = building_def.catalog.size
+		else
+			local catalog = mapblock_lib.get_catalog(building_def.catalog)
+			size = catalog:get_size()
+		end
+		return mapblock_lib.rotate_size(size, rotation)
 	end,
 	validate = function(_, building_def)
-		local catalog, err = mapblock_lib.get_catalog(building_def.catalog)
+		local catalogfilename = building_def.catalog
+		if type(building_def.catalog) == "table" then
+			catalogfilename = building_def.catalog.filename
+		end
+
+		local catalog, err = mapblock_lib.get_catalog(catalogfilename)
 		if catalog then
 			return true
 		else
