8dd92d1c14bbda6fc4cc4a0c14a505959bd92b87
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-11-10 07:31:18 +0100

    more testing

--- build.spec.lua
+++ build.spec.lua
@@ -9,25 +9,53 @@ building_lib.register_building("building_lib:dummy", {
 	placement = "dummy"
 })
 
-
 mtt.register("build", function(callback)
     local mapblock_pos = {x=0, y=0, z=0}
     local building_name = "building_lib:dummy"
-    local success, err = building_lib.do_build(
-        mapblock_pos,
-        "singleplayer",
-        building_name,
-        0,
-        function() callback() end
-    )
+    local rotation = 0
+    local playername = "singleplayer"
+
+    -- try to build
+    local success, err = building_lib.can_build(mapblock_pos, playername, building_name, rotation)
     assert(not err)
     assert(success)
 
+    -- build
+    local callback_called = false
+    success, err = building_lib.do_build(mapblock_pos, playername, building_name, rotation,
+        function() callback_called = true end
+    )
+    assert(not err)
+    assert(success)
+    assert(callback_called)
+
+    -- try to build again
+    success, err = building_lib.can_build(mapblock_pos, playername, building_name, rotation)
+    assert(err)
+    assert(not success)
+
+    -- check
     local info = building_lib.get_placed_building_info(mapblock_pos)
     assert(info.name == building_name)
-    assert(info.rotation == 0)
-    assert(info.owner == "singleplayer")
+    assert(info.rotation == rotation)
+    assert(info.owner == playername)
     assert(info.size.x == 1)
     assert(info.size.y == 1)
     assert(info.size.z == 1)
+
+    -- try to remove
+    success, err = building_lib.can_remove(mapblock_pos)
+    assert(not err)
+    assert(success)
+
+    -- remove
+    success, err = building_lib.do_remove(mapblock_pos)
+    assert(not err)
+    assert(success)
+
+    -- check
+    info = building_lib.get_placed_building_info(mapblock_pos)
+    assert(info == nil)
+
+    callback()
 end)
