53a4fdceae93880447e2866ae02509dea63ffae1
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-11-25 16:28:33 +0100

    mtt util

--- build.spec.lua
+++ build.spec.lua
@@ -1,10 +1,4 @@
 
-building_lib.register_placement("dummy", {
-    check = function() return true end,
-    get_size = function() return {x=1,y=1,z=1} end,
-    place = function(_, _, _, _, callback) callback() end
-})
-
 building_lib.register_building("building_lib:dummy", {
 	placement = "dummy"
 })
